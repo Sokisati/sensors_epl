5e343d32c294855e9c5ee07e52f866d6330e2bd7
Sokisati <yagizalpersoy@gmail.com>
2024-07-23 17:47:00 +0300

    bug fix

--- gps_file.py
+++ gps_file.py
@@ -1,33 +1,28 @@
-import gps
+import gpsd
 import time
 
 class GPSSensor:
     def __init__(self):
 
-        self.session = gps.gps(mode=gps.WATCH_ENABLE)
-        self.session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+        gpsd.connect()
         time.sleep(1);
     
     def getLat(self):
-        report = self.session.next()
-        while report['class'] != 'TPV':
-            report = self.session.next()
-        return report.lat
+
+        packet = gpsd.get_current()
+        return packet.lat
 
     def getLong(self):
-        report = self.session.next()
-        while report['class'] != 'TPV':
-            report = self.session.next()
-        return report.lon
+
+        packet = gpsd.get_current()
+        return packet.lon
 
     def getAlt(self):
-        report = self.session.next()
-        while report['class'] != 'TPV':
-            report = self.session.next()
-        return report.alt
+
+        packet = gpsd.get_current()
+        return packet.alt
 
     def test(self):
         print("Latitude:", self.getLat())
         print("Longitude:", self.getLong())
         print("Altitude:", self.getAlt())
-
